c25110502f002792a95c4b2a2e117a272c394d0d
Aphiwe <makisiaphiwe@gmail.com>
2023-09-19 18:06:43 +0200

    Added a todo model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,13 @@
+import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+  title = models.CharField(max_length=30)
+  description = models.CharField(max_length=500)
+  date_created = datetime.datetime.now()
+  due_date = models.DateField()
+
+  def __str__(self):
+    return self.title
